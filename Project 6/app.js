1a6253ea7c13af5371cc706459d9ef4828fbc284
Muhammad Usman <usman.three003@gmail.com>
2026-04-01 21:07:40 +0500

    Upload source code and materials for Project 6

---
+++ Project 6/app.js
@@ -0,0 +1,15 @@
+const getElement = (selector) =>{
+    const element = document.querySelector(selector)
+    if (element) return element
+    throw Error(`Please double check your class name, there is no ${selector} class`)
+}
+const links = getElement('.nav-links')
+const navBtn = getElement('.nav-btn')
+
+navBtn.addEventListener('click',()=>{
+    links.classList.toggle('show-links')
+})
+
+const date = getElement('#date')
+const currentYear = new Date().getFullYear()
+date.textContent = currentYear
